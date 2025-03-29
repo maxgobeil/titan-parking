db2f465f64878d505f45c7a20d7426f2e542d496
Max Gobeil <gobmaxime@gmail.com>
2025-03-28 21:37:06 -0400

    Feat: Responsive css

--- quotes/static/js/base.js
+++ quotes/static/js/base.js
@@ -5,18 +5,29 @@ document.addEventListener('DOMContentLoaded', function () {
   const mobileNav = document.getElementById('mobile-nav');
   const mobileNavLinks = document.querySelectorAll('.mobile-nav-link');
   const header = document.querySelector('.header');
+  const navLinks = document.querySelectorAll('.nav-link');
+  const langButton = document.querySelector('.lang-button');
 
   const onScroll = () => {
     if (document.body.scrollTop > 100) {
       header.classList.add('scrolled');
+      navLinks.forEach((link) => {
+        link.classList.add('scrolled');
+      });
+      langButton.classList.add('scrolled');
     } else {
       header.classList.remove('scrolled');
+      navLinks.forEach((link) => {
+        link.classList.remove('scrolled');
+      });
+      langButton.classList.remove('scrolled');
     }
   };
   // Toggle mobile menu
   function toggleMenu() {
     mobileNav.classList.toggle('open');
     document.body.classList.toggle('menu-open');
+    menuToggle.style.display = mobileNav.classList.contains('open') ? 'none' : 'block';
   }
 
   // Event listeners for menu
