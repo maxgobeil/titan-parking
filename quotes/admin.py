b4ecbac780a841be4c2a9159fe973332c6017023
Max Gobeil <gobmaxime@gmail.com>
2025-05-14 15:13:59 -0400

    Fix: Admin panel user problem

--- quotes/admin.py
+++ quotes/admin.py
@@ -21,6 +21,43 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ("email", "first_name", "last_name", "is_staff")
     ordering = ("email",)
 
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "job_title")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Business Permissions",
+            {
+                "fields": (
+                    "can_create_quotes",
+                    "can_approve_quotes",
+                    "can_access_financials",
+                )
+            },
+        ),
+        ("Social Auth", {"fields": ("is_social_account", "social_provider")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+
 
 class QuoteItemInline(admin.TabularInline):
     model = QuoteItem
